d6743ffb588a55814228e900555eb03aabedfe2f
Ilia Kaziamov <kaziamov@outlook.com>
2024-01-27 09:32:09 +0300

    Update logic and tests

--- simplecrud/crud.py
+++ simplecrud/crud.py
@@ -5,26 +5,29 @@ from typing import Dict
 from sqlalchemy import select
 
 from .settings import session
+from .utils import inject_connection
 
 logger = logging.getLogger(__name__)
 
 
-async def create_obj(model, **params):
+@inject_connection
+async def create_obj(model, params, conn=None):
     """Create object in db"""
     logger.debug(f"{__name__}.create_obj: model = {model}, params = {params}")
     new_obj = model(**params)
-    async with session() as conn:
+    async with conn:
         conn.add(new_obj)
         await conn.commit()
     return new_obj
 
 
-async def get_object(model, **filters):
+@inject_connection
+async def get_object(model, filters, conn=None):
     """Get object from db"""
     key = f"{model.__name__}{filters}"
     query = select(model).filter_by(**filters)
     logger.debug(f"{__name__}.get_obj: query = {query}")
-    async with session() as conn:
+    async with conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_obj: result = {result}")
     obj = result.scalars().first()
@@ -32,10 +35,11 @@ async def get_object(model, **filters):
     return obj
 
 
-async def get_all(model):
+@inject_connection
+async def get_all(model, conn=None):
     """Get objects from db"""
     query = select(model)
-    async with session() as conn:
+    async with conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_all: result = {result}")
     objects = result.scalars().all()
@@ -43,10 +47,11 @@ async def get_all(model):
     return objects
 
 
-async def get_all_with_filter(model, filters: dict):
+@inject_connection
+async def get_all_with_filter(model, filters: dict, conn=None):
     """Get objects from db"""
     query = select(model).filter_by(**filters)
-    async with session() as conn:
+    async with conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_all: result = {result}")
     objects = result.scalars().all()
@@ -54,11 +59,12 @@ async def get_all_with_filter(model, filters: dict):
     return objects
 
 
-async def get_objects(model, filters: Dict, limit=10, per_page=10):
+@inject_connection
+async def get_objects(model, filters: Dict, limit=10, per_page=10, conn=None):
     """Get objects from db"""
     query = select(model).filter_by(**filters).limit(limit).offset(per_page)
     logger.debug(f"{__name__}.get_objects: query = {query}")
-    async with session() as conn:
+    async with conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_objects: result = {result}")
     objects = result.scalars().all()
@@ -66,20 +72,21 @@ async def get_objects(model, filters: Dict, limit=10, per_page=10):
     return objects
 
 
-async def get_or_create_object(model, **params):
+async def get_or_create_object(model, params, conn=None):
     """Get object from db or create new one"""
     key = f"{model.__name__}{params}"
-    obj = await get_object(model, **params)
+    obj = await get_object(model, params)
     if not obj:
-        obj = await create_object(model, **params)
+        obj = await create_object(model, params)
     return obj
 
 
-async def create_object(model, **params):
+@inject_connection
+async def create_object(model, params, conn=None):
     """Create object in db"""
     logger.debug(f"{__name__}.create_obj: model = {model}, params = {params}")
     new_obj = model(**params)
-    async with session() as conn:
+    async with conn:
         conn.add(new_obj)
         await conn.commit()
     return new_obj
@@ -89,18 +96,43 @@ def create_objects():
     pass
 
 
-async def update_object(obj, **params):
-    async with session() as conn:
+@inject_connection
+async def update_object(obj, params, conn=None):
+    """
+    Soft Update object in db.
+    If attribute not exists in model`s fields, then skip field without error
+    """
+    avaliable_fields = obj.__class__.__table__.columns.keys()
+    async with conn:
         for key, value in params.items():
-            setattr(obj, key, value)
-        conn.add(obj)
+            if key in avaliable_fields:
+                setattr(obj, key, value)
         await conn.commit()
+        conn.refresh(obj)
     return obj
 
 
-async def update_object_by_id(model, id: int, **params):
+@inject_connection
+async def update_or_error(obj, params, conn=None):
+    """
+    Soft Update object in db.
+    If attribute not exists in model`s fields, then skip field without error
+    """
+    avaliable_fields = obj.__class__.__table__.columns.keys()
+    async with conn:
+        for key, value in params.items():
+            if key in avaliable_fields:
+                setattr(obj, key, value)
+            else:
+                raise AttributeError(f"Attribute {key} not exists in {obj.__class__.__name__}")
+        await conn.commit()
+        conn.refresh(obj)
+    return obj
+
+
+async def update_object_by_id(model, id: int, params, conn=None):
     obj = await get_object(model, id=id)
-    updated_obj = await update_object(obj, **params)
+    updated_obj = await update_object(obj, params)
     return updated_obj
 
 
@@ -108,13 +140,27 @@ def update_objects():
     pass
 
 
-def update_or_create_object():
-    pass
+async def update_or_create_object(model, filters, params):
+    obj = await get_or_create_object(model, filters)
+    return await update_object(obj, params)
 
 
-async def delete_object(model, id: int):
-    pass
+@inject_connection
+async def delete_object(obj, conn=None):
+    async with conn:
+        conn.delete(obj)
+        await conn.flush()
+    logger.debug(f"{__name__}.delete_object: model = {obj.__class__}, id = {obj.id}")
+    return True
 
 
-def delete_objects():
-    pass
+@inject_connection
+async def delete_object_by_id(model, id: int, conn=None):
+    async with conn:
+        conn.query(model).where(model.id == id)
+        await conn.commit()
+    return True
+
+
+async def delete_objects(objects, conn=None):
+    return False
--- simplecrud/utils.py
+++ simplecrud/utils.py
@@ -12,3 +12,21 @@ def async_to_sync(func):
         return result
 
     return wrapper
+
+
+def inject_connection(func):
+    """Decorator to inject database connection to function"""
+
+    @wraps(func)
+    def inner(*args, **kwargs):
+        from simplecrud.settings import session
+        if not kwargs.get('conn'):
+            kwargs['conn'] = session()
+        result = func(*args, **kwargs)
+        try:
+            kwargs['conn'].close()
+        except:
+            pass
+        return result
+
+    return inner
